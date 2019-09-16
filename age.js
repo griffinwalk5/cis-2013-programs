ad288f569ff336dbffba629ad7c7c343fa6208a9
griffinwalk5 <54722665+griffinwalk5@users.noreply.github.com>
2019-09-16 13:24:18 -0500

    Delete age.js

--- age.js
+++
@@ -1,9 +0,0 @@
-
-    var  floatAge, floatDays, intWeek, floatMonths, floatFortnites;
-    floatAge= parseFloat(prompt("enter your age"));
-    floatDays= floatAge * 365.25;
-    floatMonths = floatAge * 12;
-    intWeek = floatDays / 7;
-    floatFortnites = floatDays / 14;
-    alert("Initial Age = " + floatAge + " years: " + " Days lived= " + floatDays + " days: " + " Months Lived= " + floatMonths + " months: " + " Weeks Lived= " + intWeek + " weeks: " + "Fortnights Lived= " + floatFortnites + " fortnights.");
-  
