051ed1b86834220e9437f565bf72c7272022a15c
griffinwalk5 <54722665+griffinwalk5@users.noreply.github.com>
2019-11-13 21:09:01 -0600

    Update guess2.js

--- guess2.js
+++ guess2.js
@@ -85,17 +85,8 @@ while(isNaN(intGuess) || parseInt(intGuess) < intMin || parseInt(intGuess) > int
     {
         alert ("Guess is too low");
     }
-    intCount ++;
- 
-
-intGuess = prompt("Enter a guess between " + intMin + " and " + intMax);
- 
-while(isNaN(intGuess) || parseInt(intGuess) < intMin || parseInt(intGuess) > intMax)
-  {
-    intGuess = prompt ("Your guess is invalid");
+    intCount ++; 
   }
- 
-}
  
  
  
